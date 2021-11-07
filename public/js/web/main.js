29a78e11a1bf4b0d09896ef32cb4d49e2763f71d
Giuliano Criscuoli <giulianodias450@gmail.com>
2021-11-07 09:46:50 -0300

    Finalizando a primeira versão do layout

--- public/js/web/main.js
+++ public/js/web/main.js
@@ -6,13 +6,15 @@ $(function() {
 
         var dataGroup = $(this).attr('data-group');
         var portfolio = $('.portfolio-area');
+        list.removeClass('active');
 
         if(dataGroup == 'all') {
-
+            $(this).addClass('active');
             portfolio.show('1000');
 
-        } else {
+        } else {    
 
+            $(this).addClass('active');
             portfolio.not('.' + dataGroup).hide('1000');
             portfolio.filter('.' + dataGroup).show('1000');
         }
